dad69e64c97acbaa3496f2de1ed371ca9b4be082
wildlifehexagon <crossthebreeze@gmail.com>
2017-12-26 16:31:23 -0600

    set up basic table of data

---
+++ client/src/components/Table.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { AutoSizer, Table, Column } from 'react-virtualized'
+
+class App extends Component {
+    state = { users: [] }
+
+    componentDidMount() {
+        fetch('/data')
+            .then(res => res.json())
+            .then(users => this.setState({ users }))
+    }
+
+    rowGetter = ({ index }) => {
+        const data = this.state.users
+        if (data[index]) {
+            return data[index]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Users</h1>
+                {this.state.users.map(user => (
+                    <table key={user.id} style={ { width: '100%' } }>
+                        <tr>
+                            <td style={ { width: '25%' } }>{user.id}</td>
+                            <td style={ { width: '25%' } }>{user.name}</td>
+                            <td style={ { width: '25%' } }>{user.username}</td>
+                            <td style={ { width: '25%' } }>{user.email}</td>
+                        </tr>
+                    </table>
+                ))}
+
+                {/* <AutoSizer>
+                {({ height, width }) => (
+                    <Table
+                    headerHeight={30}
+                    height={height}
+                    rowCount={10}
+                    rowGetter={ this.rowGetter }
+                    rowHeight={50}
+                    width={width}
+                    >
+                        <Column
+                            label="Index"
+                        />
+                        <Column
+                            label="Name"
+                        />
+                        <Column
+                            label="Username"
+                        />
+                        <Column
+                            label="Email"
+                        />
+                    </Table>
+                )}
+                </AutoSizer> */}
+            </div>
+        )
+    }
+}
+
+export default App
