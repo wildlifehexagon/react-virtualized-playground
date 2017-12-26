1633e167de2216c587897fa6b82bbb1b1a9f66f7
wildlifehexagon <crossthebreeze@gmail.com>
2017-12-26 14:17:33 -0600

    add concurrently for dual servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = { users: [] }
+
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }))
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Users</h1>
+                {this.state.users.map(user => (
+                    <div key={user.id}>{user.username}</div>
+                ))}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
