8a8f78bb74603afb06c4b1057f6ec2a6110f1b13
wildlifehexagon <crossthebreeze@gmail.com>
2017-12-29 11:22:31 -0600

    data now displays with RV, formatting needs fixed

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { AutoSizer, Table, Column } from 'react-virtualized'
+import { Table, Column } from 'react-virtualized'
 
 class App extends Component {
     state = { users: [] }
@@ -15,49 +15,47 @@ class App extends Component {
             <div>
                 <h1>Users</h1>
                 {this.state.users.map(user => (
-                    <table key={user.id} style={ { width: '100%' } }>
+                        <Table
+                            headerHeight={50}
+                            height={630}
+                            width={1100}
+                            rowCount={this.state.users.length}
+                            rowGetter={ ({index}) => this.state.users[index] }
+                            rowHeight={100}
+                        >
+                            <Column
+                                label="ID"
+                                dataKey="id"
+                                width={ 250 }
+                            />
+                            <Column
+                                label="Name"
+                                dataKey="name"
+                                width={ 250 }
+                            />
+                            <Column
+                                label="Username"
+                                dataKey="username"
+                                width={ 250 }
+                            />
+                            <Column
+                                label="Email"
+                                dataKey="email"
+                                width={ 250 }
+                            />
+                        </Table>
+                ))}
+
+                {/* <table key={user.id} style={ { width: '100%' } }>
+                    <tbody>
                         <tr>
                             <td style={ { width: '25%' } }>{user.id}</td>
                             <td style={ { width: '25%' } }>{user.name}</td>
                             <td style={ { width: '25%' } }>{user.username}</td>
                             <td style={ { width: '25%' } }>{user.email}</td>
                         </tr>
-                    </table>
-                ))}
-
-                <AutoSizer>
-                {({ height, width }) => (
-                    <Table
-                    headerHeight={30}
-                    height={height}
-                    rowCount={10}
-                    rowGetter={ ({index}) => this.state.users[index] }
-                    rowHeight={50}
-                    width={width}
-                    >
-                        <Column
-                            label="ID"
-                            dataKey="id"
-                            width={ 250 }
-                        />
-                        <Column
-                            label="Name"
-                            dataKey="name"
-                            width={ 250 }
-                        />
-                        <Column
-                            label="Username"
-                            dataKey="username"
-                            width={ 250 }
-                        />
-                        <Column
-                            label="Email"
-                            dataKey="email"
-                            width={ 250 }
-                        />
-                    </Table>
-                )}
-                </AutoSizer>
+                    </tbody>
+                </table> */}                
             </div>
         )
     }
